2369efae9402e008ebb2af5a3ce7bdec88161a15
Arebos <lent1510@gmail.com>
2018-12-14 17:50:18 +0900

    react-ts-template

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
+import logoSvg from './logo.svg';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logoSvg} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
